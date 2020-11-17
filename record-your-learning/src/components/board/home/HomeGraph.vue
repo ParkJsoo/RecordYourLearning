b5d60bd3bafa6d534bfd440b696860716818cc12
ParkJsoo <misty0314@naver.com>
2020-11-18 05:53:21 +0900

    create main page - graph
    - 홈 화면 그래프 구현

--- record-your-learning/src/components/board/home/HomeGraph.vue
+++ record-your-learning/src/components/board/home/HomeGraph.vue
@@ -1,24 +1,33 @@
 <template>
     <div id="home-graph">
-
+      <line-chart class="canvas"></line-chart>
     </div>
 </template>
 
 <script>
-export default {
+import {Line} from 'vue-chartjs'
+import LineChart from './LineChart'
 
+export default {
+  extends: Line,
+  components: {
+    'LineChart': LineChart
+  },
 }
 </script>
 
 <style scoped>
-    #home-graph{
-        width: 550px;
-        height: 245px;
-        padding: 30px;
-        border-radius: 8px;
-        box-shadow: 0 3px 6px 0 rgba(66, 66, 66, 0.16);
-        border: solid 1px #d6d6d6;
-        background-color: #ffffff;
-        box-sizing: border-box;
-    }
+  #home-graph {
+    width: 550px;
+    height: 245px;
+    padding: 30px;
+    border-radius: 8px;
+    box-shadow: 0 3px 6px 0 rgba(66, 66, 66, 0.16);
+    border: solid 1px #d6d6d6;
+    background-color: #ffffff;
+    box-sizing: border-box;
+  }
+  .canvas {
+    height: 200px;
+  }
 </style>
---
+++ record-your-learning/src/components/board/home/LineChart.vue
@@ -0,0 +1,79 @@
+<script>
+import { Line } from 'vue-chartjs'
+
+export default {
+  extends: Line,
+    data () {
+      return {
+        gradient: null,
+        // datacollection: {
+        //     labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov'], 
+        //     datasets: [
+        //         {
+        //             backgroundColor: this.gradient,
+        //             pointBackgroundColor: '#24b6ba',
+        //             borderWidth: 2,
+        //             borderColor: "#24b6ba",
+        //             data: [35, 55, 75, 50, 55]
+        //         }
+        //     ]
+        // },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        display: false,
+                        beginAtZero: true,
+                        stepSize: 20,
+                    },
+                    gridLines: {
+                        display: true,
+                        zeroLineColor: "#24b6ba",
+                        zeroLineWidth: 2,
+                    }
+                }],
+                xAxes: [ {
+                    ticks:{
+						fontColor : "#707070",
+                        fontSize : 14,
+					},
+                    gridLines: {
+                        display: false,
+                    }
+                }]
+            },
+            legend: {
+                display: false
+            },
+            responsive: true,
+            maintainAspectRatio: false
+            }
+        }
+    },
+    mounted () {
+        this.gradient = this.$refs.canvas.getContext('2d').createLinearGradient(0, 0, 0, 450)
+
+        this.gradient.addColorStop(0, 'rgba(36, 182, 186, 0.3)')
+        this.gradient.addColorStop(0.5, 'rgba(18, 91, 93, 0)')
+        this.gradient.addColorStop(1, 'rgba(255, 255, 255, 0)');
+
+        this.renderChart({
+            labels: ['Jul', 'Aug', 'Sep', 'Oct', 'Nov'], 
+            datasets: [
+                {
+                    backgroundColor: this.gradient,
+                    pointBackgroundColor: '#24b6ba',
+                    borderWidth: 2,
+                    borderColor: "#24b6ba",
+                    data: [35, 55, 75, 50, 55],
+                }
+            ]
+        }, this.options)
+    }
+}
+</script>
+
+
+<style>
+
+</style>
